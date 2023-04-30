d859e7460230c59d4de8c0771780b4f05a475138
Andrew Rutherfoord <andrew.rutherfoord@live.com>
2023-05-01 01:53:56 +0200

    Cleaned up a bit

--- background/background.js
+++ background/background.js
@@ -2,17 +2,15 @@ import { getHost, getHostConfigData, setHostTextData } from "../utils.js";
 
 async function handleText(text) {
   let host = await getHost();
-  console.log(host);
-  console.log(text);
   setHostTextData(host, text);
 }
 
 chrome.downloads.onDeterminingFilename.addListener(async function (item, suggest) {
+  // Retrieve the current tab's configs
   const config = await getHostConfigData(await getHost());
-  console.log(config);
+  
+  // Only block if activated and the file extension is one of the allowed
   if (config.activate && config.filetypes.some((end) => item.filename.endsWith(end))) {
-    // if (config.activate && item.mime === "text/markdown") {
-    console.log(suggest);
     chrome.downloads.cancel(item.id);
     fetch(item.finalUrl)
       .then((response) => response.text())
--- popup/popup.js
+++ popup/popup.js
@@ -1,18 +1,17 @@
 import {
   updateClipboard,
   getHost,
-  retrieveHostData,
   getHostConfigData,
   setHostConfigData,
   getHostTextData,
 } from "../utils.js";
 
 async function copyContent() {
-  // let host = await getHost();
-  // let content = await retrieveHostData(host);
+  // Copy the contents of the text area in the popup to the clipboard.
   updateClipboard(contentTextArea.value);
 }
 
+// Handles config form submission.
 async function submitConfigForm(event) {
   event.preventDefault();
 
@@ -32,16 +31,20 @@ const copyButton = document.getElementById("copy-button");
 copyButton.addEventListener("click", copyContent);
 
 const form = document.getElementById("config-form");
-form.addEventListener("submit", submitConfigForm);
 const filetypesCheckboxes = form.querySelectorAll("input[type=checkbox][name=filetypes]");
 const activateCheckbox = form.querySelectorAll("input[type=checkbox][name=activate]")[0];
+form.addEventListener("submit", submitConfigForm);
 
 const contentTextArea = document.getElementById("content");
+
 getHost().then((host) => {
+  // Load the text data that has been stored by the background script.
   getHostTextData(host).then((data) => {
-    console.log(data)
+    console.log(data);
     contentTextArea.value = data;
   });
+
+  // Load config data and render to form.
   getHostConfigData(host).then((data) => {
     activateCheckbox.checked = data.activate;
     console.log(data);
