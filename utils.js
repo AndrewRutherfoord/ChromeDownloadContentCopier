887b942c42a3464e9cf2b1d252921aa58f566d9e
Andrew Rutherfoord <andrew.rutherfoord@live.com>
2023-05-01 17:13:18 +0200

    Fixed background.js crashing because of faulty import

--- utils.js
+++ utils.js
@@ -1,4 +1,22 @@
-import { retrieveHostData, setHostData } from "./storage";
+export async function retrieveData() {
+  let data = await chrome.storage.local.get(["copyFileDownloadContents"]);
+  return data.copyFileDownloadContents ? data.copyFileDownloadContents : {};
+}
+
+export async function setData(data) {
+  chrome.storage.local.set({ copyFileDownloadContents: data });
+}
+
+export async function retrieveHostData(host) {
+  let data = await retrieveData();
+  return data[host] ? data[host] : {};
+}
+
+export async function setHostData(host, data) {
+  let result = await retrieveData();
+  result[host] = data;
+  setData(result);
+}
 
 // Get the host of the active tab (if url https://example.com/test, host is example.com)
 export async function getHost() {
